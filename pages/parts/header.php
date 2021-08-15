42ec8a7118c469bd35153b17a9a75709ab4fa698
Bruno Nunes <bruno.nunes@moneycloud.com.br>
2021-08-15 02:24:54 -0300

    Fix some styles

--- pages/parts/header.php
+++ pages/parts/header.php
@@ -20,7 +20,7 @@ require('function/helper.php');
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/"><i class="fas fa-store"></i> <?= $app['site_name'] ?></a>
+        <a class="navbar-brand text-primary" href="/"><i class="fas fa-store"></i> <?= $app['site_name'] ?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -37,9 +37,9 @@ require('function/helper.php');
         </div>
     </div>
 </nav>
-<div class="container-fluid">
+<div class="container-fluid p-5">
     <div class="row">
-        <div class="col py-4">
-            <h1 class="text-center text-secondary"><?= $page_title ?></h1>
+        <div class="col pb-3">
+            <h1 class="text-center text-primary"><?= $page_title ?></h1>
         </div>
     </div>
