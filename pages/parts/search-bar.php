33a51b153da42754e740d6d922fb9a0a9b00186e
Bruno Nunes <bruno.nunes@moneycloud.com.br>
2021-08-15 00:54:54 -0300

    Left previous search in search bar

--- pages/parts/search-bar.php
+++ pages/parts/search-bar.php
@@ -1,6 +1,6 @@
     <form class="row w-100">
         <div class="col-9">
-            <input type="text" name="s" class="search w-100 p-1" id="search" placeholder="Encontrar produto...">
+            <input type="text" name="s" class="search w-100 p-1" id="search" value="<?=($_GET['s']??'')?>" placeholder="Encontrar produto...">
         </div>
         <div class="col-3">
             <button type="submit" class="btn btn-primary w-100 ">Pesquisar</button>
