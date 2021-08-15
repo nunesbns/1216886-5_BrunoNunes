6e02dad16cf949fa057f98db9179b174677bb766
Bruno Nunes <bruno.nunes@moneycloud.com.br>
2021-08-14 23:45:14 -0300

    Add search bar

---
+++ pages/parts/search-bar.php
@@ -0,0 +1,8 @@
+    <form class="row w-100">
+        <div class="col-9">
+            <input type="text" name="s" class="search w-100 p-1" id="search" placeholder="Encontrar produto...">
+        </div>
+        <div class="col-3">
+            <button type="submit" class="btn btn-primary w-100 ">Pesquisar</button>
+        </div>
+    </form>
