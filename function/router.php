92169e85a221ea52aaf14891124c9ba30aefd491
Bruno Nunes <bruno.nunes@moneycloud.com.br>
2021-08-14 21:31:49 -0300

    Add function to handle routes

---
+++ function/router.php
@@ -0,0 +1,27 @@
+<?php
+
+if(!function_exists('load_content')) {
+    /**
+     * Load content according to uri
+     */
+    function load_content(){
+        $uri = $_SERVER["REQUEST_URI"];
+        $path = array_filter(explode('/', $uri)) ?? ['home'];
+
+        switch (reset($path)) {
+            case 'home':
+                include_once('pages/index.php');
+                break;
+            case 'quem-somos':
+                include_once('pages/page.php');
+                break;
+            case 'contato':
+                include_once('pages/contato.php');
+                break;
+            case 'produto':
+                include_once('pages/single.php');
+                break;
+            default: include_once('pages/404.php');
+        }
+    }
+}
