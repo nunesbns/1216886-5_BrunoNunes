1c3173796e2e69bf600ebe9169dd0eb1d9d56d45
Bruno Nunes <bruno.nunes@moneycloud.com.br>
2021-08-24 03:25:31 -0300

    Fix styles order for local overwriting

--- pages/parts/header.php
+++ pages/parts/header.php
@@ -10,12 +10,12 @@ require('function/helper.php');
 <head>
     <meta charset="utf-8">
     <title><?= $page_title . " | " . $app['site_name']; ?></title>
-    <link href="css/style.css" rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
           integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link href="css/style.css" rel="stylesheet"/>
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-light bg-light ">
